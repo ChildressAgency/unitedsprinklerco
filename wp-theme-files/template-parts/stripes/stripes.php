325cd3661ded242cba2d635c84b2a1b88de062c0
Mikhail <mmccarty@childressagency.com>
2019-08-26 23:40:59 -0400

    configurable background colors for the stripes.

--- wp-theme-files/template-parts/stripes/stripes.php
+++ wp-theme-files/template-parts/stripes/stripes.php
@@ -4,12 +4,15 @@
     the_row();
     $image_size = get_sub_field("image_size");
     $image_alignment = get_sub_field("image_alignment");
+    $color = get_sub_field("color");
     ?>
-    <div class="<?php echo $image_size == "small" ? "container" : "container-fluid" ?> stripe py-3">
-      <div class="row align-items-stretch">
-        <?php if (get_sub_field("image_position") == "left") include("image.php") ?>
-        <?php include("text.php") ?>
-        <?php if (get_sub_field("image_position") == "right") include("image.php") ?>
+    <div class="stripe <?php echo $color ?>">
+      <div class="<?php echo $image_size == "small" ? "container" : "container-fluid" ?>">
+        <div class="row align-items-stretch">
+          <?php if (get_sub_field("image_position") == "left") include("image.php") ?>
+          <?php include("text.php") ?>
+          <?php if (get_sub_field("image_position") == "right") include("image.php") ?>
+        </div>
       </div>
     </div>
   <?php endwhile ?>
