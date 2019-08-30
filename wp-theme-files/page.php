5255f832dbfd70acbf01b47837d35730b0e99edb
Mikhail <mmccarty@childressagency.com>
2019-08-30 02:17:04 -0400

    let pages have columns on the bottom - only used in About, for now

--- wp-theme-files/page.php
+++ wp-theme-files/page.php
@@ -15,7 +15,25 @@
     </article>
   <?php
   endwhile;
-endif;
+endif; ?>
+
+<?php if (have_rows("columns")): ?>
+  <div class="footer-columns"
+       style="background-image: url('<?php echo get_field("background_image")["sizes"]["large"] ?>')">
+    <div class="backdrop">
+      <div class="container">
+        <div class="row text-center">
+          <?php while (have_rows("columns")): the_row() ?>
+            <div class="col-12 col-md-4">
+              <h3 class="text-center"><?php echo get_sub_field("title") ?></h3>
+              <div><?php echo get_sub_field("body") ?></div>
+            </div>
+          <?php endwhile ?>
+        </div>
+      </div>
+    </div>
+  </div>
+<?php endif;
 
 get_template_part("template-parts/testimonials/slider");
 
