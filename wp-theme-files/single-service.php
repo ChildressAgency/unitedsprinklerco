4b95ee50466e349b9515482157b50fb31ef24212
Mikhail <mmccarty@childressagency.com>
2019-08-31 03:59:30 -0400

    links to other services

--- wp-theme-files/single-service.php
+++ wp-theme-files/single-service.php
@@ -5,7 +5,39 @@
 <?php get_template_part("template-parts/waves") ?>
 
   <div id="content" class="single-service wide">
-    <?php get_template_part("template-parts/page/content") ?>
+<?php get_template_part("template-parts/page/content") ?>
+
+<?php
+$currentID = get_the_ID();
+$services = new WP_Query(array(
+  'post_type' => 'service',
+  'post_status' => 'publish',
+  'posts_per_page' => -1,
+  'order' => 'ASC'
+));
+?>
+
+<?php if ($services->have_posts()): ?>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 text-center">
+        <h3 class="pb-2">More Services</h3>
+      </div>
+
+      <?php while ($services->have_posts()): $services->the_post() ?>
+        <?php if (get_the_ID() != $currentID): ?>
+          <a class="col-12 col-md-3 service text-center pt-2" href="<?php the_permalink() ?>">
+            <img class="img-fluid" src="<?php echo get_field("icon") ?>"
+                 alt="<?php echo get_the_title() ?>"/>
+            <h4 class="pt-2"><?php the_title() ?></h4>
+          </a>
+        <?php endif; ?>
+      <?php endwhile; ?>
+    </div>
+  </div>
+  </div>
+<?php endif; ?>
+
   </div>
 
 <?php get_template_part("template-parts/stripes/stripes") ?>
