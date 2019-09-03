b6d43185b299695957237248c871cdfda1b270fa
Mikhail <mmccarty@childressagency.com>
2019-09-03 01:06:45 -0400

    front page: services list

--- wp-theme-files/front-page.php
+++ wp-theme-files/front-page.php
@@ -1,4 +1,13 @@
-<?php get_header(); ?>
+<?php
+get_header();
+
+$services = new WP_Query(array(
+  'post_type' => 'service',
+  'post_status' => 'publish',
+  'posts_per_page' => -1,
+  'order' => 'ASC'
+));
+?>
 
   <div class="stripe page-header front-page-header">
     <div class="container-fluid">
@@ -32,6 +41,28 @@
 
   <div id="content">
     <?php get_template_part("template-parts/page/content") ?>
+
+    <?php if ($services->have_posts()): ?>
+      <div class="container-fluid">
+        <div class="row services front-page-services px-md-5 mx-md-5 justify-content-center align-items-start">
+          <?php while ($services->have_posts()): $services->the_post() ?>
+            <a class="col-12 col-md service text-center" href="<?php the_permalink() ?>">
+              <img class="img-fluid" src="<?php echo get_field("icon") ?>"
+                   alt="<?php echo get_the_title() ?>"/>
+              <h4 class="pt-2"><?php the_title() ?></h4>
+              <p class="pt-3">
+                <?php echo get_field("summary") ?>
+              </p>
+            </a>
+          <?php endwhile; ?>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <a class="button" href="<?php echo get_permalink(get_page_by_path("services")) ?>">Services</a>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
   </div>
 
 <?php get_template_part("template-parts/stripes/stripes") ?>
