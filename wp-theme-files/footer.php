abca571fd1c45db00b5d18a0fcf07889f8b72eaf
Mikhail <mmccarty@childressagency.com>
2019-09-05 00:23:42 -0400

    it is helpful to echo things that should be output.

--- wp-theme-files/footer.php
+++ wp-theme-files/footer.php
@@ -7,7 +7,7 @@
     <div class="row">
 
       <div class="col-12 col-md-6">
-        <a href="<?php get_site_url() ?>">
+        <a href="<?php echo get_site_url() ?>">
           <img src="<?php echo get_template_directory_uri() ?>/img/logo-white.png" alt="Logo" class="img-fluid"/>
         </a>
       </div>
