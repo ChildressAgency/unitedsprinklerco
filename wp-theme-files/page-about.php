fd1c745899e586be42dc5caa90ba6b010f4bb3cc
Mikhail <mmccarty@childressagency.com>
2019-09-05 00:19:14 -0400

    about page has no contact form.

---
+++ wp-theme-files/page-about.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<?php get_template_part("template-parts/page/header") ?>
+
+<?php get_template_part("template-parts/waves") ?>
+
+  <div id="content">
+    <?php get_template_part("template-parts/page/content") ?>
+  </div>
+
+<?php get_template_part("template-parts/stripes/stripes") ?>
+
+<?php get_template_part("template-parts/page/columns");
+
+get_template_part("template-parts/testimonials/slider");
+
+get_footer();
